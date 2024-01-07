436289cbb4a058c5a1a47471f2c32fce10c4f09e
Joel Newman <joellnewman@hotmail.com>
2024-01-06 17:21:19 -0700

    nope, adding it to the __init__ didn't solve the issue

--- webservers/rpi/handle_web_commands.py
+++ webservers/rpi/handle_web_commands.py
@@ -5,6 +5,14 @@ from pathlib import Path
 import time
 
 
+import os, sys
+
+# Add the root directory to the Python path
+current_directory = os.path.dirname(os.path.abspath(__file__))
+webservers_directory = os.path.abspath(os.path.join(current_directory, ".."))
+sys.path.append(webservers_directory)
+
+
 from common.common_objects import setup_common_logger, all_standard_column_names
 
 logger = logging.getLogger("web_commands")
