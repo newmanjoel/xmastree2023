78fcaeafc30b39598d17d761676cf007261c7e87
Joel Newman <joellnewman@hotmail.com>
2024-01-12 18:25:34 -0700

    lets just try without the \n

--- webservers/rpi/main.py
+++ webservers/rpi/main.py
@@ -83,10 +83,11 @@ if __name__ == "__main__":
             time.sleep(1)
             total_running_time_s += 1
             if total_running_time_s % 120 == 0:
-                if not config.log_capture.getvalue().endswith(
-                    r"press ctrl+c to stop\n"
-                ):
-                    logger.getChild("main_loop").info("press ctrl+c to stop")
+                logger.debug(
+                    f"{config.log_capture.getvalue().endswith('press ctrl+c to stop')=}"
+                )
+                # if not config.log_capture.getvalue().endswith("press ctrl+c to stop"):
+                logger.getChild("main_loop").info("press ctrl+c to stop")
                 config.log_capture.truncate(100_000)
     except KeyboardInterrupt:
         stop_event.set()
