584e0577fabd84ca07a3e42a027d63e0d01e23de
Joel Newman <joellnewman@hotmail.com>
2024-01-12 18:11:27 -0700

    for some reason the logging is going ...

--- webservers/rpi/commands.py
+++ webservers/rpi/commands.py
@@ -44,9 +44,10 @@ def handle_fps(*, value: float, **kwargs) -> None:
 def handle_brightness(*, value: float, display_queue: queue.Queue, **kwargs) -> None:
     limit = lambda x: min(max(float(x), 0.0), 1.0)
     config.brightness = limit(value)  # type: ignore
-    config.pixels.setBrightness(config.brightness * 255)  # type: ignore
+    scaled_brightness = int(config.brightness * 255)
+    config.pixels.setBrightness(scaled_brightness)  # type: ignore
     logger.getChild("set_brightness").debug(
-        f"setting brightness to {config.brightness}/{config.brightness*255}"
+        f"setting brightness to {config.brightness}/{scaled_brightness}"
     )
     # this triggers a refresh of the current dataframe.
     # display_queue.put(config.current_dataframe)
